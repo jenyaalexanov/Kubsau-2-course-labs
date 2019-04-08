c4d54e556678dd73b68751fe46cb97b539d6ee96
JenyaAlexanov <jenyaalex99@gmail.com>
2019-04-08 09:11:14 +0300

    + lab3

---
+++ lab3/Form1.cs
@@ -0,0 +1,95 @@
+﻿using lab3.Helper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<string> a = File.ReadAllLines("Files/a.txt"
+               .ToApplicationPath())
+               .ToList();
+                List<string> b = File.ReadAllLines("Files/b.txt"
+                    .ToApplicationPath())
+                    .ToList();
+
+                var orderA = a
+                    .OrderBy(x => x.Count())
+                    .ToList();
+                var orderB = b
+                    .OrderBy(x => x.Count())
+                    .ToList();
+
+                var longA = orderA.Last();
+
+                a[a.IndexOf(longA)] = orderB.First();
+                b[b.IndexOf(orderB.First())] = longA;
+
+                File.WriteAllLines("Files/a.txt"
+                    .ToApplicationPath(), a);
+                File.WriteAllLines("Files/b.txt"
+                    .ToApplicationPath(), b);
+                MessageBox.Show("Готово!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка! {ex.Message}");
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<string> a = File.ReadAllLines("Files/a.txt"
+               .ToApplicationPath())
+               .ToList();
+                List<string> b = File.ReadAllLines("Files/b.txt"
+                    .ToApplicationPath())
+                    .ToList();
+
+                var orderA = a
+                    .OrderBy(x => x.Count())
+                    .ToList();
+                var orderB = b
+                    .OrderBy(x => x.Count())
+                    .ToList();
+
+                var longB = orderB.Last();
+
+
+                b[b.IndexOf(longB)] = orderA.First();
+                a[a.IndexOf(orderA.First())] = longB;
+
+                File.WriteAllLines("Files/a.txt"
+                    .ToApplicationPath(), a);
+                File.WriteAllLines("Files/b.txt"
+                    .ToApplicationPath(), b);
+                MessageBox.Show("Готово!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка! {ex.Message}");
+            }
+        }
+    }
+}
---
+++ lab3/Helper/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace lab3.Helper
+{
+    public static class Helper
+    {
+        public static string ToApplicationPath(this string fileName)
+        {
+            var exePath = Path.GetDirectoryName(System.Reflection
+                                .Assembly.GetExecutingAssembly().CodeBase);
+            Regex appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+            var appRoot = appPathMatcher.Match(exePath).Value;
+            return Path.Combine(appRoot, fileName);
+        }
+    }
+}
